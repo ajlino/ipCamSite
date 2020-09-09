c15e067d449f8316d0a3ee089ee5cf753402de94
ajlino <38694859+ajlino@users.noreply.github.com>
2020-09-09 13:37:12 -0400

    Initial Commit

---
+++ myJavascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#clickMe").click(function(){
+    // alert ("Button Clicked!!");
+    console.log("Button Clicked!!");
+    // $("#imageDiv").removeClass("hide");
+    $("#imageDiv").addClass("center");
+  });
+});
